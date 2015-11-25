960305780c226d3190a61dc789d2fe23e42fedd4
Antoine Daudenthun <daudenthun.a@laposte.net>
2015-11-25 22:34:47 +0100

    creation de la liste dans index.php + css mineur

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<header><title>SliderX - Antoine Daudenthun</title></header>
+<link rel="stylesheet" type="text/css" href="style-sliderx.css">
+<body>
+	<ul>
+		<li><img src="images/un.jpg"></li>
+		<li><img src="images/deux.jpg"></li>
+		<li><img src="images/trois.jpg"></li>
+		<li><img src="images/quatre.jpg"></li>
+	</ul>
+</body>
+</html>
